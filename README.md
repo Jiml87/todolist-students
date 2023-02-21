d2bf8b4e802feece2ae969063efa3bb7e7ac0b6c
Slavik Semenenko <vsemenenko@mdsol.com>
2023-02-21 18:36:59 +0200

    sorting

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # todolist-students
+
+#### Install
+
+`npm i http-server -g`
+
+#### Run
+
+`http-server`
+
+#### Open
+
+`http://localhost:8080`
--- src/index.js
+++ src/index.js
@@ -29,10 +29,24 @@ function setState(newState) {
   render();
 }
 
-function render() {
-  const { data, editedId } = state;
-  const listElement = document.querySelector("#list");
-  listElement.innerHTML = data
+const sortAZ = () => state.data.sort((a, b) => (a.text > b.text ? 1 : -1));
+
+const sortZA = () => state.data.sort((a, b) => (b.text > a.text ? 1 : -1));
+
+const sortingMethod = (target) => {
+  if (target.innerText === "A-Z") {
+    sortAZ();
+    target.innerText = "Z-A";
+  } else {
+    sortZA();
+    target.innerText = "A-Z";
+  }
+
+  render();
+};
+
+const renderDueToPriority = (data, editedId) =>
+  data
     .map((item, index) => {
       const isEdit = editedId && item.id === editedId;
       return `<li class="list-group-item d-flex justify-content-between align-items-center">
@@ -74,6 +88,27 @@ function render() {
       </li>`;
     })
     .join("");
+
+const getDataByPriority = (priority) => (list) => list.priority === priority;
+
+function render() {
+  const { data, editedId } = state;
+  const listLowElement = document.querySelector("#list-low");
+  const listMediumElement = document.querySelector("#list-medium");
+  const listHighElement = document.querySelector("#list-high");
+
+  listLowElement.innerHTML = renderDueToPriority(
+    data.filter(getDataByPriority("low")),
+    editedId
+  );
+  listMediumElement.innerHTML = renderDueToPriority(
+    data.filter(getDataByPriority("middle")),
+    editedId
+  );
+  listHighElement.innerHTML = renderDueToPriority(
+    data.filter(getDataByPriority("high")),
+    editedId
+  );
 }
 
 function addTask() {
@@ -147,8 +182,8 @@ document.querySelector(".task-button").addEventListener("click", () => {
   addTask();
 });
 
-document.addEventListener("click", (event) => {
-  const target = event.target;
+document.addEventListener("click", (ev) => {
+  const target = ev.target;
   if (target.classList.contains("delete-button")) {
     showDeleteWindow(target);
   }
@@ -167,6 +202,9 @@ document.addEventListener("click", (event) => {
   }
   if (target.classList.contains("remove-wind-button")) {
     deleteTask();
+  }
+  if (target.classList.contains("sort")) {
+    sortingMethod(target);
   }
 });
 
